7659043ba43a4a451add395a3b5cbadc41b35877
Артём Салимов <arsa-17@bk.ru>
2024-09-17 21:25:14 +0500

    Update module_1_3.py

--- module_1_3.py
+++ module_1_3.py
@@ -2,8 +2,8 @@ name='Artem'
 print('Name:',name)
 age=32
 print('Age:',age)
-new_age='1'
-int(new_age)
-print('New Age:', age+int(new_age))
-is_student='True'
+age = age + 1
+int(age)
+print('New Age:', age)
+is_student=True
 print('Is Student:', is_student)
